ba6251609db28d86fc0030bb5453ee63f9d2c2f2
warturtletcg <warturtletcg@gmail.com>
2021-11-19 13:03:50 -0800

    Week 5 Project 1 v2

--- gradeclass.cpp
+++ gradeclass.cpp
@@ -6,17 +6,17 @@ using namespace std;
 int main(){
     int GradeOne, GradeTwo;
 
-    cout<<"please enter 2 grades, seperated by a space:"<<endl;
+    //cout<<"please enter 2 grades, seperated by a space:"<<endl;
     cin>>GradeOne>>GradeTwo;
 
     if(GradeOne < 60 && GradeTwo < 60){
-        cout<<"student failed:(";
+        cout<<"student failed:("<<endl;
     }
     else if(GradeOne >= 95 && GradeTwo >= 95){
-        cout<<"student graduated with honors:)";
+        cout<<"student graduated with honors:)"<<endl;
     }
     else{
-        cout<<"student graduated!";
+        cout<<"student graduated!"<<endl;
     }
     return 0;
 }
